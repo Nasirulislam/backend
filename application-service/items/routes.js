80b498fae428181775b314f9f50cd3346818a3d3
David Cordero <david@corderoramirez.com>
2018-08-31 00:45:35 +0200

    Require auth to update items

--- application-service/items/routes.js
+++ application-service/items/routes.js
@@ -7,7 +7,7 @@ module.exports = (router, itemsControllers) => {
     router.get('/items', itemsControllers.fetchItems);
     router.get('/items/:identifier', itemsControllers.fetchSingleItem);
     router.post('/items', auth, itemsControllers.insertItem);
-    router.put('/items/:identifier', itemsControllers.updateItem);
+    router.put('/items/:identifier', auth, itemsControllers.updateItem);
     router.delete('/items/:identifier', auth, itemsControllers.deleteItem);
 
     return router;
