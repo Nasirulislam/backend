43fbbbf3dbbd4b104bdcbc85b42440df50d94f71
David Cordero <david@corderoramirez.com>
2018-09-29 13:52:45 +0200

    Support username/password and email/password logins

--- application-service/accounts/controllers/loginController.js
+++ application-service/accounts/controllers/loginController.js
@@ -5,13 +5,17 @@ const Joi = require('joi');
 const Account = require('../models/account');
 
 const loginController = function(req, res) {
-
-    const schema = {
-        email: Joi.string().email().required(), 
-        password: Joi.string().required()
-    };
  
+    const schema = Joi.object()
+        .keys({
+            username: Joi.string(),
+            email: Joi.string().email(),
+            password: Joi.string().required()
+        })
+        .xor('username', 'email');
+
     const credentials = {
+        username: req.body.username,
         email: req.body.email,
         password: req.body.password
     };
@@ -19,7 +23,10 @@ const loginController = function(req, res) {
     Joi.validate(credentials, schema, async function(error, value) {
         if (error) {
             let path = error.details[0].path[0];
-            if (path === 'email') {
+            if (path === 'username') {
+                return res.status(400).send({ code: 9 });
+            }
+            else if (path === 'email') {
                 return res.status(400).send({ code: 10 });
             }
             else if (path === 'password') {
@@ -30,19 +37,26 @@ const loginController = function(req, res) {
             }
         }
 
-        const accountByEmail = await Account.where('email', value.email).fetch();
-        if (!accountByEmail) {
+        let account;
+        if (value.email) {
+            account = await Account.where('email', value.email).fetch();
+        }
+        else if (value.username) {
+            account = await Account.where('username', value.username).fetch();
+        }
+
+        if (!account) {
             return res.status(400).json({ code: 1 });
         }
 
-        let validPassword = await bcrypt.compare(value.password, accountByEmail.get('password'));
+        let validPassword = await bcrypt.compare(value.password, account.get('password'));
         if (!validPassword) {
             return res.status(400).json({ code: 1 });
         }
 
         const token = jwt.sign({ 
-            id: accountByEmail.get('id'),
-            username: accountByEmail.get('username')
+            id: account.get('id'),
+            username: account.get('username')
         }, process.env.JWT_SECRET);
 
         res.status(200).json({ token });
