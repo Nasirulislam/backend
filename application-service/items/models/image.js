1eeb8257549ce6fb057ced32bcf1c64488674cb9
David Cordero <david@corderoramirez.com>
2018-10-02 23:50:04 +0200

    Add path to image responses

--- application-service/items/models/image.js
+++ application-service/items/models/image.js
@@ -4,7 +4,13 @@ const bookshelf = require('../../db/bookshelf');
 
 const Image = bookshelf.Model.extend({
     tableName: 'images',
-    hidden: ['id', 'item_id']
+    hidden: ['id', 'item_id'],
+
+    toJSON: function() {
+        var attrs = bookshelf.Model.prototype.toJSON.apply(this, arguments);
+        attrs.path = process.env.IMAGES_BASE_URL;
+        return attrs;
+    }
 });
 
 module.exports = Image;
