0dae38887e6c17606165d4b068945fb24e470435
David Cordero <david@corderoramirez.com>
2018-10-09 00:17:35 +0200

    Add test for invalid old and new password parameters

--- application-service/tests/integration/accounts/changePassword.test.js
+++ application-service/tests/integration/accounts/changePassword.test.js
@@ -25,5 +25,39 @@ describe('/api/accounts/password', () => {
             expect(res.status).toBe(401);
             expect(res.body.code).toBe(3);
         });
+
+        it('should return error when given old password is not valid', async () => {
+            // Given
+            let token = jwt.sign({ id: 2 }, process.env.JWT_SECRET);
+            let parameters = { 
+                old: 12345,
+                password: 'valid' };
+
+            // When
+            const res = await request(server)
+                .post('/api/accounts/password').send(parameters)
+                .set('x-auth-token', token);
+
+            // Then
+            expect(res.status).toBe(400);
+            expect(res.body.code).toBe(11);
+        });
+
+        it('should return error when given new password is not valid', async () => {
+            // Given
+            let token = jwt.sign({ id: 2 }, process.env.JWT_SECRET);
+            let parameters = { 
+                old: 'valid',
+                password: 12345 };
+
+            // When
+            const res = await request(server)
+                .post('/api/accounts/password').send(parameters)
+                .set('x-auth-token', token);
+
+            // Then
+            expect(res.status).toBe(400);
+            expect(res.body.code).toBe(13);
+        });
     });
 });
