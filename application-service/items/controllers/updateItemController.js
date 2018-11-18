83527a4999f0ee5cf4d612a5a19bd3e776eb0c99
David Cordero <david@corderoramirez.com>
2018-11-18 22:06:59 +0100

    Validate input in update item controller

--- application-service/items/controllers/updateItemController.js
+++ application-service/items/controllers/updateItemController.js
@@ -5,11 +5,48 @@ const Item = require('../models/item');
 
 const updateItemController = function(req, res) {
 
-    const schema = { identifier: Joi.number() };
-    const identifier = req.params.identifier;
-    Joi.validate({ identifier }, schema, async function(error, value) {
-        if(error) {
-            return res.status(404).send({ code: 2 });
+    const schema = {
+        identifier: Joi.number(),
+        title: Joi.string().trim().max(100).min(4),
+        description: Joi.string().trim().max(1000).min(10),
+        location_id: Joi.number().min(1).max(26),
+        images: Joi.array().items(Joi.string().regex(/^(.*)\.png/)),
+        author_id: Joi.number().required(),
+    };
+
+    const item = {
+        identifier: req.params.identifier,
+        title: req.body.title,
+        description: req.body.description,
+        location_id: req.body.location_id,
+        images: req.body.images,
+        author_id: req.user.id,
+    };
+
+    Joi.validate(item, schema, async function(error, value) {
+        if (error) {
+            let path = error.details[0].path[0];
+            if (path === 'identifier') {
+                return res.status(404).send({ code: 2 });
+            }
+            if (path === 'title') {
+                return res.status(400).send({ code: 5 });
+            }
+            else if (path === 'description') {
+                return res.status(400).send({ code: 6 });
+            }
+            else if (path === 'images') {
+                return res.status(400).send({ code: 12 });
+            }
+            else if (path === 'location_id') {
+                return res.status(400).send({ code: 19 });
+            }
+            if (path === 'author_id') {
+                return res.status(404).send({ code: 3 });
+            }
+            else {
+                return res.status(400).send({ code: error });
+            }
         }
 
         const itemById = await Item.where('id', value.identifier).fetch();
@@ -17,15 +54,32 @@ const updateItemController = function(req, res) {
             return res.status(404).send({ code: 2 });
         }
 
-        if (itemById.get('author_id') === req.user.id) {
-            itemById.save(
-                {
-                    title: req.body.title,
-                    description: req.body.description
-                })
-                .then(function(saved) {
-                    res.json({ saved });
-                });
+        if (itemById.get('author_id') === value.author_id) {
+
+            if (value.title) {
+                itemById.title = value.title;
+            }
+
+            if (value.description) {
+                itemById.description = value.description;
+            }
+
+            if (value.location_id) {
+                itemById.location_id = value.location_id;
+            }
+
+            if (value.images) {
+                itemById.images = value.images;
+            }
+
+            const savedItem = await itemById.save();
+
+            if (savedItem) {
+                return res.status(200).json({ item: item });
+            }
+            else {
+                return res.status(500).json({});
+            }
         }
         else {
             return res.status(401).send({ code: 4 });
